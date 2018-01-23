f819c342a30f43e3efed40b89967ddab96d2b944
VadimNi <vadim_ni@mail.ru>
2018-01-23 10:32:54 +0300

    first commit

--- a.sh
+++ a.sh
@@ -6,7 +6,7 @@
 #mkdir public uploads etc
 #cd public
 #mkdir img css js
-apt-get update && apt-get install -y nginx mc curl && 
-rm /etc/nginx/sites-available/default && 
-ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default && 
-/etc/init.d/nginx restart
+#apt-get update && apt-get install -y nginx mc curl && 
+sudo rm /etc/nginx/sites-available/default && 
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default && 
+sudo /etc/init.d/nginx restart
